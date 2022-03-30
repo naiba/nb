3ba7f97a1ca72cabb53692f46a008acf66611b1d
naiba <hi@nai.ba>
2022-03-30 14:59:11 +0800

    fix: `nb -gu github flutter pub get`

--- cmd/flutter.go
+++ cmd/flutter.go
@@ -13,14 +13,10 @@ var flutterCmd = &cli.Command{
 	Usage:           "Enhanced flutter command.",
 	SkipFlagParsing: true,
 	Action: func(c *cli.Context) error {
-		var args []string
-		if c.Args().Len() > 0 {
-			args = append(args, c.Args().Slice()[1:]...)
-		}
 		_, env, err := GetGitSSHCommandEnv(c.String("git-user"), c.String("proxy"))
 		if err != nil {
 			return cli.Exit(err.Error(), 1)
 		}
-		return ExecuteInHost(env, "flutter", args...)
+		return ExecuteInHost(env, "flutter", c.Args().Slice()...)
 	},
 }
