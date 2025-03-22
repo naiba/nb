28cc4e69f2a958770245f43829fe5cfa5d33697a
naiba <hi@nai.ba>
2025-03-22 15:21:43 +0800

    feat(anchor): naming env & switching env

--- cmd/anchor.go
+++ cmd/anchor.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -20,14 +21,71 @@ var anchorCmd = &cli.Command{
 	Name:  "anchor",
 	Usage: "Anchor helper.",
 	Subcommands: []*cli.Command{
-		changeEnvCmd,
+		switchingEnvCmd,
+		namingEnvCmd,
 	},
 }
 
-var changeEnvCmd = &cli.Command{
-	Name:            "env",
-	Aliases:         []string{"e"},
-	Usage:           "change anchor env.",
+var namingEnvCmd = &cli.Command{
+	Name:            "naming-env",
+	Aliases:         []string{"ne"},
+	Usage:           "Naming anchor env.",
+	SkipFlagParsing: true,
+	Action: func(c *cli.Context) error {
+		envName := c.Args().First()
+		if envName == "" {
+			return errors.New("env name is required")
+		}
+		currentPath, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		currentPath += "/target/deploy/"
+		log.Println("Current path: ", currentPath)
+		envKeySuffix := fmt.Sprintf(".%s.json", envName)
+		var matchedKeys []string
+		filepath.WalkDir(currentPath, func(path string, d os.DirEntry, err error) error {
+			fileName := d.Name()
+			if strings.HasSuffix(fileName, "-keypair.json") {
+				log.Printf("Found env file: %s", fileName)
+				matchedKeys = append(matchedKeys, fileName)
+			}
+			return nil
+		})
+		if len(matchedKeys) == 0 {
+			return fmt.Errorf("no matched env file(%s) found", envKeySuffix)
+		}
+		for _, key := range matchedKeys {
+			baseName := strings.TrimSuffix(key, filepath.Ext(key))
+			baseName += envKeySuffix
+			if _, err := os.Stat(filepath.Join(currentPath, baseName)); err == nil {
+				log.Printf("Env file(%s) already exists", baseName)
+				continue
+			}
+			envKeyFile, err := os.Create(filepath.Join(currentPath, baseName))
+			if err != nil {
+				log.Fatalf("Failed to create env file: %v", err)
+			}
+			defer envKeyFile.Close()
+			oldKeyFile, err := os.Open(filepath.Join(currentPath, key))
+			if err != nil {
+				log.Fatalf("Failed to open env file: %v", err)
+			}
+			defer oldKeyFile.Close()
+			_, err = io.Copy(envKeyFile, oldKeyFile)
+			if err != nil {
+				log.Fatalf("Failed to copy env file: %v", err)
+			}
+			log.Printf("Env file(%s) created", baseName)
+		}
+		return nil
+	},
+}
+
+var switchingEnvCmd = &cli.Command{
+	Name:            "switching-env",
+	Aliases:         []string{"se"},
+	Usage:           "Switching anchor env.",
 	SkipFlagParsing: true,
 	Action: func(c *cli.Context) error {
 		envName := c.Args().First()
@@ -61,10 +119,10 @@ var changeEnvCmd = &cli.Command{
 			}
 			baseName += ".json"
 			if err := os.Remove(filepath.Join(currentPath, baseName)); err != nil {
-				return err
+				log.Fatalf("Failed to remove env file: %v", err)
 			}
 			if err := os.Symlink(filepath.Join(currentPath, key), filepath.Join(currentPath, baseName)); err != nil {
-				return err
+				log.Fatalf("Failed to Symlink env file: %v", err)
 			}
 		}
 
